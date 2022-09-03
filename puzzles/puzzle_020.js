e9aaa041f386ff1fed0d110e4a0c60198aa73fba
Aeliriannon <iriska8807@gmail.com>
2022-09-03 18:02:28 +0400

    changed name of argument for function

--- puzzles/puzzle_020.js
+++ puzzles/puzzle_020.js
@@ -1,11 +1,11 @@
-//Напишите код для вычисления факториала числа
+// Напишите код для вычисления факториала числа
 
 const num = 5;
-  function fuctorial(a) {  
-  	if(a === 0) {
+  function fuctorial(x) {  
+  	if(x === 0) {
      return 1;     
     }
-    return a * fuctorial(a-1);
+    return x * fuctorial(x-1);
 	}	
   
   console.log(fuctorial(num))
